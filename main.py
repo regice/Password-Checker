6899299507a32f36d73dad9622b99ea8d3d88c42
Brandon Tran <brandonquangtran@gmail.com>
2023-05-30 19:53:27 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import hashlib
+import sys
+
+
+# Request data from API and return response
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(f"Error fetching: {response.status_code}, check the API and try again.")
+
+    return response
+
+
+# Takes a password hash's tail and checks to see if it exists in hashes received from API
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+# Hashes password and checks it with API response data
+def pwned_api_check(password):
+    # Check if password exists in API response
+    sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1_password[:5], sha1_password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f"{password} was found {count} times... you should probably change your password...")
+        else:
+            print(f"{password} was NOT found. Carry on!")
+    return 'Done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
